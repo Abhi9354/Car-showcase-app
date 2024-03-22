378f18a44b496c54a9f80d30448db5c964e3e0ce
knowledge <abhi7ty4@gmail.com>
2024-03-23 00:17:59 +0530

    hero

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -22,7 +22,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -60,7 +60,7 @@ const SearchManufacturer = ({
                     }
                     value={item}
                   >
-                    {({selective,active})=>(
+                    {({selected,active})=>(
                       <>
                       <span
                           className={`block truncate ${
