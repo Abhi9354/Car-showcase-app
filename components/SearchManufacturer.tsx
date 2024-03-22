f92d9b9d1352c33299c525bbea045fb298273177
knowledge <abhi7ty4@gmail.com>
2024-03-23 00:13:08 +0530

    hero

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -60,7 +60,26 @@ const SearchManufacturer = ({
                     }
                     value={item}
                   >
-                    {({selective,active})=>{}}
+                    {({selective,active})=>{
+                      <>
+                      <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                           
+                          </span>
+                        ) : null}
+                      </>
+                    }}
                   </Combobox.Option>
                 )
               )}
