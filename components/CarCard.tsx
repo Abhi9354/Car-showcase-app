7696612995fa805605b4b8f71553f6706f4b169f
knowledge <abhi7ty4@gmail.com>
2024-03-24 22:58:11 +0530

    hero

---
+++ components/CarCard.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { CarProps } from "@/types"
+import { calculateCarRent } from "@/utils";
+import Image from "next/image";
+
+interface CarcardProps{
+  car:CarProps;
+}
+const CarCard = ({car}:CarcardProps) => {
+  const {city_mpg,year,make,model,transmission,drive}=car;
+
+  const carRent=calculateCarRent(city_mpg, year)
+  return (
+    <div className="car-card group">
+     <div className="car-card__content">
+      <h2 className="car-card__content-title">{make}{model}</h2>
+     </div>
+     <p className="flex mt-6 text-[32px] font-extrabold">
+      <span className="self-start text-[14px] font-semibold" >
+        $
+      </span>
+      {carRent}
+      <span className="self-start text-[14px] font-medium" >
+        /day
+      </span>
+     </p>
+     <div className="relative w-full h-40 my-3 object-contain">
+   <Image src="/hero.png" alt="car model" fill priority className="object-contain"/>
+     </div>
+    </div>
+  )
+}
+
+export default CarCard
