a5331b58ae45c8314d9157944601a721318c1038
knowledge <abhi7ty4@gmail.com>
2024-03-27 00:25:18 +0530

    hero

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CarProps } from "@/types";
 import { Fragment } from "react";
 import Image from "next/image";
@@ -11,7 +13,11 @@ const CarDetails = ({isOpen,closeModel,car}:CarDetailsProps) => {
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-
+<Dialog as="div" className="relative z-10" onClose={closeModel}>
+<Transition.Child>
+    <div className="fixed inset-0 bg-black bg-opacity-25"></div>
+</Transition.Child>
+</Dialog>
       </Transition>
     </>
   )
