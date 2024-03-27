e35f8e320efb1689d0d3e935042f9af5e62ee907
knowledge <abhi7ty4@gmail.com>
2024-03-27 23:45:34 +0530

    hero

--- utils/index.ts
+++ utils/index.ts
@@ -25,13 +25,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-export const generateCarImageUrl=(car:CarProps,angle?:string)=>{
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
 
-    const url=new URL('https://cdn.imagin.studio/getimage');
-    const {make,model}=car;
-    url.searchParams.append('customer','harry-potter');
-    url.searchParams.append('make',make);
-    url.searchParams.append('model',model);
-    url.searchParams.append('year',`${car.year}`);
-    url.searchParams.append('angle',`${angle}`);
-}
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
