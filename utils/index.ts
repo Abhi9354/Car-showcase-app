ed061f9e8d2c79eb0b26f07d1438e0fd2e966a82
knowledge <abhi7ty4@gmail.com>
2024-03-28 00:30:41 +0530

    hero

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export const fetchCars = async() => {
         'X-RapidAPI-Key': '3a7c5d49a2mshf86f04a624678efp1a01a7jsn69aabb6dd7e4',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
       }
-      const response= await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',{headers:headers},)
+      const response= await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',{headers:headers},)
       const results= await response.json();
       return results;
 
@@ -29,7 +29,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('customer', 'hrjavascript-mastery');
   url.searchParams.append('make', make);
   url.searchParams.append('modelFamily', model.split(" ")[0]);
   url.searchParams.append('zoomType', 'fullscreen');
